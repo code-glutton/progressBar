e7e9de606b76a0fd0bf76232dc4d294f4e78a9a8
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-16 23:22:19 +0100

    assigning value to css variable solved

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ let inputVar = document.getElementsByTagName("input");
 console.log(inputVar);
 
 var arr = Array.from(inputVar);
-let currentNum = getComputedStyle(document.documentElement)
-    .getPropertyValue('--progress');
+
      
 arr.map(x => {
     console.log("osas");
@@ -12,15 +11,15 @@ arr.map(x => {
         let progressNum = 20;
         let currentNum = getComputedStyle(document.documentElement)
     .getPropertyValue('--progress');
-     
+    console.log(currentNum + " num2");
     let parsedCurrent = parseInt(currentNum);
-    console.log(parsedCurrent);
+    console.log(parsedCurrent + " parsed Current");
 
     let sum = progressNum + parsedCurrent;
-    console.log(sum);
+    console.log(sum + " sum");
     document.documentElement.style
     .setProperty('--progress', `${sum}%`);
+    console.log(getComputedStyle(document.documentElement)
+    .getPropertyValue('--progress') + " num");
     })
 });
-
-console.log(currentNum);
