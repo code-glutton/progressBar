3b5d5773e481d5b78738c6c304ed5c3ec48b55df
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-17 21:17:50 +0100

    corrected the progress bar always extending out of the parent div

--- js/script.js
+++ js/script.js
@@ -10,18 +10,25 @@ var arr = Array.from(inputVar);
 arr.map(x => {
     console.log(x);
     x.addEventListener("change", () => {
-        let progressNum = 20;
+        let progressNum = 25;
         let currentNum = getComputedStyle(document.documentElement)
     .getPropertyValue('--progress');
+    
     console.log(currentNum + " num2");
     let parsedCurrent = parseInt(currentNum);
-    console.log(parsedCurrent + " parsed Current");
+    if (parsedCurrent < 100){
+        console.log(parsedCurrent + " parsed Current");
 
-    let sum = progressNum + parsedCurrent;
-    console.log(sum + " sum");
-    document.documentElement.style
-    .setProperty('--progress', `${sum}%`);
-    console.log(getComputedStyle(document.documentElement)
-    .getPropertyValue('--progress') + " num");
+        let sum = progressNum + parsedCurrent;
+        console.log(sum + " sum");
+        document.documentElement.style
+        .setProperty('--progress', `${sum}%`);
+        console.log(getComputedStyle(document.documentElement)
+        .getPropertyValue('--progress') + " num");
+        }
+        else{
+            console.log("full");
+        }
     })
+    
 });
